ed072cbe7d2d01bc2147ae2a84eccf8d8fc35c84
waynh77 <waynh77@gmail.com>
2021-04-16 00:42:50 +0700

    Upload

---
+++ TesCam/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.AxVLCPlugin21 = New AxAXVLC.AxVLCPlugin2()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        CType(Me.AxVLCPlugin21, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AxVLCPlugin21
+        '
+        Me.AxVLCPlugin21.Enabled = True
+        Me.AxVLCPlugin21.Location = New System.Drawing.Point(12, 12)
+        Me.AxVLCPlugin21.Name = "AxVLCPlugin21"
+        Me.AxVLCPlugin21.OcxState = CType(resources.GetObject("AxVLCPlugin21.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxVLCPlugin21.Size = New System.Drawing.Size(320, 240)
+        Me.AxVLCPlugin21.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(338, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(322, 240)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(15, 262)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(316, 20)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "http://192.168.1.3:4747/mjpegfeed?320x240"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(252, 295)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(78, 27)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "On"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(566, 265)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(93, 30)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Capture"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(80, 332)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(190, 277)
+        Me.ListBox1.TabIndex = 5
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox2.Location = New System.Drawing.Point(337, 332)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(322, 240)
+        Me.PictureBox2.TabIndex = 6
+        Me.PictureBox2.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(674, 625)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.AxVLCPlugin21)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Test Ip Cam"
+        CType(Me.AxVLCPlugin21, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents AxVLCPlugin21 As AxAXVLC.AxVLCPlugin2
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ TesCam/Form1.vb
@@ -0,0 +1,60 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        isiListFile()
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        PictureBox2.BackgroundImage = Bitmap.FromFile(Application.StartupPath & "\gbr\" & ListBox1.SelectedItem.ToString)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Button1.Text = "On" Then
+            Button1.Text = "Off"
+
+            AxVLCPlugin21.playlist.add(TextBox1.Text)
+            AxVLCPlugin21.playlist.play()
+        Else
+            Button1.Text = "On"
+            AxVLCPlugin21.playlist.stop()
+
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Clipboard.Clear()
+        AxVLCPlugin21.video.takeSnapshot()
+        Dim tgljam As String = Now
+        Dim sFile = Dir(Application.StartupPath & "\*.bmp")
+        Dim filebMp As String = ""
+        Do Until sFile = ""
+            filebMp = sFile
+            sFile = Dir()
+        Loop
+
+        Dim Filename As String = Application.StartupPath & "\" & filebMp
+        Dim NewFileName As String = Application.StartupPath & "\gbr\" & Format(Now, "yyyyMMddHHmmss") & ".bmp"
+
+        My.Computer.FileSystem.CopyFile(Filename, NewFileName)
+        My.Computer.FileSystem.DeleteFile(Filename)
+        If Dir$(NewFileName) <> "" Then
+            PictureBox1.BackgroundImage = Bitmap.FromFile(NewFileName)
+        End If
+
+        isiListFile()
+    End Sub
+
+    Private Sub isiListFile()
+        ' make a reference to a directory
+        Dim di As New IO.DirectoryInfo(Application.StartupPath & "\gbr")
+        Dim diar1 As IO.FileInfo() = di.GetFiles()
+        Dim dra As IO.FileInfo
+        ListBox1.Items.Clear()
+        'list the names of all files in the specified directory
+        For Each dra In diar1
+            ListBox1.Items.Add(dra)
+        Next
+    End Sub
+End Class
